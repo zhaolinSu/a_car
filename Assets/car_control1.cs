daf04f5de7795216d44000716d9c17a408123b82
zhaolinSu <39791337+zhaolinSu@users.noreply.github.com>
2018-06-30 20:52:37 +0800

    Initial commit

---
+++ Assets/car_control1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_control1 : MonoBehaviour {
+    void Awake()
+    {
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+            Screen.SetResolution(Display.displays[i].renderingWidth, Display.displays[i].renderingHeight, true);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+            this.transform.Translate(-50 * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+            this.transform.Translate(50 * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+            this.transform.Translate(0, 0, -10 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            this.transform.Translate(0, 0, 10 * Time.deltaTime);
+
+        float rotationSpeed = 30.0F;
+        float rotation = 1;
+        if (Input.GetKey(KeyCode.G))
+        {
+            rotation = -1 * rotationSpeed;
+            rotation *= Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+        }
+        if (Input.GetKey(KeyCode.H))
+        {
+            rotation = 1 * rotationSpeed;
+            rotation *= Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+        }
+
+        print("Rotate" + this.transform.localEulerAngles);
+        print("X azumith:" + this.transform.localEulerAngles.x);
+        print("coordinates" + this.transform.localPosition);
+        print("X axis:" + this.transform.localPosition.x);
+    
+
+}
+}
---
+++ Assets/car_poistion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_poistion : MonoBehaviour {
+
+	
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print("Rotate" + this.transform.localEulerAngles);
+        print("X azumith:" + this.transform.localEulerAngles.x);
+        print("coordinates" + this.transform.localPosition);
+        print("X axis:" + this.transform.localPosition.x);
+
+    }
+}
